96d0ee2541448c7c68e309028468785c73da6e83
Aishani-Roy <aishaniroy2202@gmail.com>
2026-05-29 15:29:51 +0530

    py

---
+++ check.py
@@ -0,0 +1,5 @@
+wlist=[True,'hi',1.3,True]
+if wlist[0]==wlist[-1]:
+    print("first and last value match")
+else:
+    print("they do not match")  
---
+++ list.py
@@ -0,0 +1,7 @@
+alist=[(1,3,5,7),(2,4,6,8)]
+print(alist)
+print(len(alist))
+alist.reverse()
+print(alist)
+blist=[1,2,3]*3
+print(blist)
